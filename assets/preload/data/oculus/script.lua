cbae42014485249332a98cc5a4cbb8924e071d21
newbion10 <96935020+newbion10@users.noreply.github.com>
2022-10-31 18:28:26 +0900

    Add files via upload

---
+++ assets/preload/data/oculus/script.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bfhalloween');
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx');
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver');
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd');
+	setProperty('gf.visible', false)
+end	
